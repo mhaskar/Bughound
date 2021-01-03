76ad8ed2517a99af8c8179be1c55933c3a2bf78e
mhaskar <m.askar@isecur1ty.org>
2021-01-03 02:22:37 +0200

    Add config file

---
+++ core/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+# Please don't change these options if you will use the docker image
+
+
+elastic_host = "http://localhost:9200/"
+
+kibana_host = "http://localhost:5601/"
+
+default_index = "findings"
